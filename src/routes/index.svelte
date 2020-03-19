ffc40217b6fee71a0ab0675cf1a4d1e1161ad50e
Devessier <baptiste@devessier.fr>
2020-03-19 23:50:16 +0100

    improvement: start to redesign index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,24 @@
+<script>
+    import GithubIcon from '../components/GithubIcon.svelte';
+</script>
+
 <svelte:head>
     <title>Accueil</title>
 </svelte:head>
 
-<div
-    class="px-16 text-10xl break-all flex flex-grow items-center justify-center
-    text-center">
-    Développeur Web
+<div class="px-16 mt-10">
+    <h1 class="text-2xl font-bold uppercase">Baptiste Devessier</h1>
+
+    <p class="mt-4 font-medium text-lg">Développeur Web, Paris</p>
+
+    <ul class="flex items-center mt-3">
+        <li>
+            <a
+                href="https://github.com/Devessier"
+                class="text-red-500"
+                title="Github">
+                <GithubIcon />
+            </a>
+        </li>
+    </ul>
 </div>
