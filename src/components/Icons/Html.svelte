763a6307006d7053d90534d6bf5c170a59ce0568
Devessier <baptiste@devessier.fr>
2020-03-20 19:59:25 +0100

    fix: delete `title` tag on HTML icon SVG

--- src/components/Icons/Html.svelte
+++ src/components/Icons/Html.svelte
@@ -4,7 +4,6 @@
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class={className}>
-    <title>HTML5 Logo Badge</title>
     <path fill="#E34F26" d="M71,460 L30,0 481,0 440,460 255,512" />
     <path fill="#EF652A" d="M256,472 L405,431 440,37 256,37" />
     <path
