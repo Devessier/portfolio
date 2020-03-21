bd51d17c8eb6eebac06b5ba660ddbd9a4ad44eb3
Devessier <baptiste@devessier.fr>
2020-03-21 17:05:45 +0100

    improvement(my-way): fade elements progressively

--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -1,9 +1,14 @@
 <script>
+    import { fade } from 'svelte/transition';
+
     export let events = [];
 
     let className;
 
     export { className as class };
+
+    const FADE_DURATION = 200;
+    const FADE_INTERVAL = 100;
 </script>
 
 <style>
@@ -19,10 +24,16 @@
 </style>
 
 <ul class={className}>
-    {#each events as { title, description, date }}
-        <p class="text-red-500 font-semibold text-left md:text-right">{date}</p>
+    {#each events as { title, description, date }, index}
+        <p
+            class="text-red-500 font-semibold text-left md:text-right"
+            in:fade={{ duration: FADE_DURATION, delay: FADE_INTERVAL * index }}>
+            {date}
+        </p>
 
-        <div class="ml-0 md:ml-4 mb-6">
+        <div
+            class="ml-0 md:ml-4 mb-6"
+            in:fade={{ duration: FADE_DURATION, delay: FADE_INTERVAL * index }}>
             <slot name="event" {title} {description} />
         </div>
     {/each}
