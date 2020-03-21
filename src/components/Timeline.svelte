c3e1b6a3cb4db59226433f1ec8d1358098d353b0
Devessier <baptiste@devessier.fr>
2020-03-21 16:44:15 +0100

    feat: begin my-way page

---
+++ src/components/Timeline.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let events = [];
+
+    let className;
+
+    export { className as class };
+</script>
+
+<style>
+    ul {
+        @apply grid;
+
+        grid-template-columns: 1fr;
+
+        @screen md {
+            grid-template-columns: max-content 1fr;
+        }
+    }
+</style>
+
+<ul class={className}>
+    {#each events as { title, description, date }}
+        <p class="text-red-500 font-semibold text-left md:text-right">{date}</p>
+
+        <div class="ml-0 md:ml-4 mb-6">
+            <slot name="event" {title} {description} />
+        </div>
+    {/each}
+</ul>
--- src/routes/my-way.svelte
+++ src/routes/my-way.svelte
@@ -1,7 +1,45 @@
 <script>
     import Page from '../components/Page.svelte';
+    import Timeline from '../components/Timeline.svelte';
+
+    const events = [
+        {
+            title: 'École 42',
+            description: 'Formation C/Unix, Web',
+            date: "2018 - aujourd'hui",
+        },
+        {
+            title: 'Bac Général Spécialité Scientifique - Mention Bien',
+            description: 'Lycée Maurice Rondeau, Bussy Saint Georges',
+            date: '2018',
+        },
+        {
+            title: 'Découverte du Web',
+            description: 'Je réalise ma première page Web',
+            date: 'Septembre 2015',
+        },
+        {
+            title: 'Brevet des Collèges - Mention Très bien',
+            description: 'Collège Sainte-Thérèse, Couilly-Pont-aux-Dames',
+            date: '2015',
+        },
+    ];
 </script>
 
-<Page>
-    <p>my-way</p>
+<svelte:head>
+    <title>Parcours</title>
+</svelte:head>
+
+<Page class="mt-10">
+    <h1 class="text-2xl font-bold uppercase">Parcours</h1>
+
+    <Timeline {events} class="md:max-w-3xl mx-auto mt-10">
+        <div slot="event" let:title let:description>
+            <h2 class="font-medium">{title}</h2>
+
+            {#if description}
+                <p class="text-sm text-gray-800 mt-1">{description}</p>
+            {/if}
+        </div>
+    </Timeline>
 </Page>
