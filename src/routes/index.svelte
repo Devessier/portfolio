202fdc2537e9ee15d0d6d2cb275c119c6ec45801
Devessier <baptiste@devessier.fr>
2020-03-24 12:22:12 +0100

    improvement: begin skills reworking

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,6 @@
 <script>
     import Page from '../components/Page.svelte'
-    import {
-        HtmlIcon,
-        CssIcon,
-        GithubIcon,
-        MailIcon,
-        TailwindIcon,
-        VueIcon,
-        NuxtIcon,
-        SvelteIcon,
-        SapperIcon,
-        VuetifyIcon,
-        NodeIcon,
-        GraphQLIcon,
-        PolkaIcon,
-        TypeScriptIcon,
-        JavaScriptIcon,
-        GolangIcon,
-    } from '../components/Icons'
+    import { GithubIcon, MailIcon } from '../components/Icons'
 
     const communicationMeans = [
         {
@@ -32,109 +15,46 @@
         },
     ]
 
-    const skills = [
-        {
-            title: 'HTML',
-            link: 'https://html.spec.whatwg.org/multipage/',
-            icon: HtmlIcon,
-        },
-        {
-            title: 'CSS',
-            link: 'https://www.w3.org/Style/CSS/#specs',
-            icon: CssIcon,
-        },
-
-        {
-            title: 'JavaScript',
-            link: 'https://tc39.es/ecma262/',
-            icon: JavaScriptIcon,
-        },
-        {
-            title: 'TypeScript',
-            link: 'https://www.typescriptlang.org/',
-            icon: TypeScriptIcon,
-        },
-
-        {
-            title: 'Vue.js',
-            link: 'https://vuejs.org',
-            icon: VueIcon,
-        },
-        {
-            title: 'Vuetify',
-            link: 'https://vuetifyjs.com',
-            icon: VuetifyIcon,
-        },
-        {
-            title: 'Nuxt.js',
-            link: 'https://nuxtjs.org',
-            icon: NuxtIcon,
-        },
-
-        {
-            title: 'Svelte',
-            link: 'https://svelte.dev',
-            icon: SvelteIcon,
-        },
-        {
-            title: 'Sapper',
-            link: 'https://sapper.svelte.dev',
-            icon: SapperIcon,
-        },
-
-        {
-            title: 'TailwindCSS',
-            link: 'https://tailwindcss.com',
-            icon: TailwindIcon,
-        },
-
-        {
-            title: 'Node.js',
-            link: 'https://nodejs.org',
-            icon: NodeIcon,
-        },
-        {
-            title: 'GraphQL',
-            link: 'https://graphql.org/',
-            icon: GraphQLIcon,
-        },
-        {
-            title: 'Polka',
-            link: 'https://github.com/lukeed/polka',
-            icon: PolkaIcon,
-        },
-
-        {
-            title: 'Golang',
-            link: 'https://go.dev/',
-            icon: GolangIcon,
-        },
+    const categories = [
+        [
+            'Front-end',
+            [
+                'HTML, CSS',
+                'JavaScript, ES6',
+                'Vue.js, Nuxt.js',
+                'Svelte, Sapper',
+                'TailwindCSS',
+                'Sass / SCSS / PostCSS',
+                'Vuetify',
+                'GraphQL',
+            ],
+        ],
+        [
+            'Back-end',
+            [
+                'Node.js',
+                'TypeScript',
+                'GraphQL',
+                'Polka',
+                'Golang',
+                'PostgreSQL',
+                'Redis',
+                'RabbitMQ',
+            ],
+        ],
+        ['Outils', ['Git', 'Docker', 'Bash / ZSH / Fish', 'macOS']],
     ]
-
-    function generatePaddingClasses(number) {
-        const classes = [
-            'p-4 md:p-4',
-            'p-3 md:p-4',
-            'p-4 md:p-4',
-            'p-3 md:p-4',
-            'p-3 md:p-4',
-        ]
-
-        return classes[number % classes.length]
-    }
-
-    function generateSizeClasses(number) {
-        const classes = [
-            'w-12 md:w-16 h-12 md:h-16',
-            'w-6 md:w-10 h-6 md:h-10',
-            'w-12 md:w-16 h-12 md:h-16',
-            'w-10 md:w-12 h-10 md:h-12',
-            'w-6 md:w-10 h-6 md:h-10',
-        ]
-
-        return classes[number % classes.length]
-    }
 </script>
+
+<style type="text/postcss">
+    .skills {
+        columns: 1;
+
+        @screen md {
+            columns: 2;
+        }
+    }
+</style>
 
 <svelte:head>
     <title>Accueil</title>
@@ -170,30 +90,21 @@
         </ul>
     </div>
 
-    <div class="mt-12 mb-6">
-        <p class="text-2xl font-bold uppercase mb-10 text-center">
-            Mes compétences
-        </p>
+    <div class="mt-16 mb-6">
+        <!-- <h2 class="text-2xl font-bold uppercase mb-10">Mes compétences</h2> -->
 
-        <ul
-            class="flex justify-center items-center flex-wrap mb-6 w-full
-            md:w-5/6 lg:w-4/5 xl:w-8/12 mx-auto">
-            {#each skills as { title, link, icon }, index}
-                <li class="mb-2 md:mb-4 mx-2 sm:mx-4 md:mx-8">
-                    <a
-                        href={link}
-                        {title}
-                        class="block transition-transform duration-300 ease-out
-                        origin-center transform shadow-xl rounded-full flex
-                        items-center justify-center hover:scale-110 {generatePaddingClasses(index)}">
-                        <span class="sr-only">{title}</span>
+        {#each categories as [title, skills]}
+            <div
+                class="grid grid-cols-1 md:grid-cols-3 pt-4 mb-4 border-t-2
+                border-red-500">
+                <h3 class="text-2xl mb-4">{title}</h3>
 
-                        <svelte:component
-                            this={icon}
-                            class={generateSizeClasses(index)} />
-                    </a>
-                </li>
-            {/each}
-        </ul>
+                <ul class="md:col-span-2 skills">
+                    {#each skills as skill}
+                        <li class="text-lg mb-2">— {skill}</li>
+                    {/each}
+                </ul>
+            </div>
+        {/each}
     </div>
 </Page>
