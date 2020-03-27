6b63bf53417ad9b492175a3d9f9fbb10d333aa8a
Devessier <baptiste@devessier.fr>
2020-03-27 17:46:44 +0100

    feat: create a component to insert json-ld scripts

---
+++ src/components/JsonLD.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let schema
+
+    $: stringified = JSON.stringify(schema)
+    $: tag = `
+        <script type="application/json-ld">
+            ${stringified + '<'}/script>
+    `
+</script>
+
+<svelte:head>
+    {@html tag}
+</svelte:head>
