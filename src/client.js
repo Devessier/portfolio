a258d17649aecbfd7e3e545176b63492bad63d82
Devessier <baptiste@devessier.fr>
2020-03-28 00:53:45 +0100

    feat: lazy load all other routes after first load

--- src/client.js
+++ src/client.js
@@ -1,5 +1,14 @@
 import * as sapper from '@sapper/app'
 
-sapper.start({
-    target: document.querySelector('#sapper'),
-})
+sapper
+    .start({
+        target: document.querySelector('#sapper'),
+    })
+    .then(() => {
+        console.log('> client-side app has been started')
+
+        return sapper.prefetchRoutes()
+    })
+    .then(() => {
+        console.log('> prefetched all routes')
+    })
