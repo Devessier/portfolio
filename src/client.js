2f32965a205da9bc683b4c2258ea5a587c40d7d1
Devessier <baptiste@devessier.fr>
2020-03-28 02:03:27 +0100

    improvement: don't prefetch routes directly
    Wait 200ms before prefetching other routes.
    This will limit the amount of made requests.

--- src/client.js
+++ src/client.js
@@ -1,14 +1,25 @@
 import * as sapper from '@sapper/app'
 
-sapper
-    .start({
+function resolveAfter(ms, value) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(value)
+        }, ms)
+    })
+}
+
+async function load() {
+    await sapper.start({
         target: document.querySelector('#sapper'),
     })
-    .then(() => {
-        console.log('> client-side app has been started')
 
-        return sapper.prefetchRoutes()
-    })
-    .then(() => {
-        console.log('> prefetched all routes')
-    })
+    console.log('> client-side app has been started')
+
+    await resolveAfter(200)
+
+    await sapper.prefetchRoutes()
+
+    console.log('> prefetched all routes')
+}
+
+load()
