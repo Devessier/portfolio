47490746edbaaf17d1356d00428a9ac26e84350d
Devessier <baptiste@devessier.fr>
2021-05-22 19:51:16 +0200

    feat: create a single page to show blog posts and add filter capacity

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,6 +1,51 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	interface ArticlePreview {
+		title: string;
+		date: string;
+		formattedDate: string;
+		slug: string;
+	}
+
+	export const load: Load = async function load({ page: { params } }) {
+		console.log(params);
+
+		const availableTopics: string[] = ['Front-end', 'Back-end', 'XState', 'Temporal'];
+
+		const articles: ArticlePreview[] = [
+			{
+				title: 'Generate and put in context or compute from context externally XState',
+				slug: slugify('Generate and put in context or compute from context externally XState', {
+					lower: true
+				}),
+				date: '2021-05-22T15:29:15.789Z',
+				formattedDate: new Intl.DateTimeFormat('en-US', {
+					day: '2-digit',
+					month: 'long',
+					year: 'numeric'
+				}).format(new Date('2021-05-22T15:29:15.789Z'))
+			}
+		];
+
+		return {
+			props: {
+				availableTopics,
+				articles
+			}
+		};
+	};
+</script>
+
 <script lang="ts">
 	import Page from '$lib/Page/Page.svelte';
-	import { LogoIcon } from '$lib/Icons';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+	import slugify from 'slugify';
+	import urlcat from 'urlcat';
+
+	export let availableTopics: string[];
+	export let articles: ArticlePreview[];
 
 	const title = 'Baptiste Devessier - Writing';
 	const description = 'Développeur Web Full Stack sur Paris';
@@ -24,71 +69,89 @@
 
 	interface Topic {
 		title: string;
-		link: string;
+		slug: string;
 		isActive: boolean;
 	}
 
-	const topics: Topic[] = [
-		{
-			title: 'Everything',
-			link: '/blog',
-			isActive: true
-		},
-		{
-			title: 'Front-end',
-			link: '/blog/topic/front-end',
-			isActive: false
-		},
-		{
-			title: 'Back-end',
-			link: '/blog/topic/back-end',
-			isActive: false
-		},
-		{
-			title: 'XState',
-			link: '/blog/topic/xstate',
-			isActive: false
-		},
-		{
-			title: 'Temporal',
-			link: '/blog/topic/temporal',
-			isActive: false
-		}
-	];
+	$: currentUrl = $page.path;
+	$: currentFilters = $page.query.get('q')?.split(',') ?? [];
 
-	const articles = [];
+	let topics: Topic[];
+	$: topics = availableTopics.map((topic) => {
+		const sluggifiedTopic = slugify(topic, {
+			lower: true
+		});
+
+		return {
+			title: topic,
+			slug: sluggifiedTopic,
+			isActive: currentFilters.includes(sluggifiedTopic)
+		};
+	});
+
+	async function handleFilterClick(slug: string) {
+		const isFilterActive = currentFilters.includes(slug);
+		const newQueryParams = isFilterActive
+			? currentFilters.filter((activeFilter) => activeFilter !== slug)
+			: [...currentFilters, slug];
+
+		if (newQueryParams.length === 0) {
+			await goto(currentUrl, {
+				replaceState: true
+			});
+
+			return;
+		}
+
+		await goto(
+			urlcat(currentUrl, {
+				q: newQueryParams
+			}),
+			{
+				replaceState: true
+			}
+		);
+	}
 </script>
 
 <Page class="pb-6" {title} {description} {canonical} {schemas} {facebook} {twitter}>
 	<h1 class="text-5xl font-cursive">Writing</h1>
 
-	<div class="pb-4 mt-8 lg:mt-12 flex items-center overflow-x-auto snap snap-x snap-mandatory">
-		{#each topics as { title, link, isActive }, index}
-			<a
-				href={link}
+	<div class="pb-4 pt-1 mt-8 lg:mt-12 flex items-center overflow-x-auto snap snap-x snap-mandatory">
+		{#each topics as { title, slug, isActive }, index}
+			<button
 				class="{isActive
 					? 'bg-red-50 hover:bg-red-100 text-red-700 border border-red-500'
 					: 'border-gray-300 border text-gray-800 bg-gray-50 hover:bg-gray-100'} transition-colors duration-100 ease-out font-medium text-sm px-3 py-1 rounded-2xl flex-shrink-0 {index ===
 				0
 					? 'ml-0'
 					: 'ml-2 lg:ml-3'} snap-start"
+				on:click={() => handleFilterClick(slug)}
 			>
 				{title}
-			</a>
+			</button>
 		{/each}
 	</div>
 
-	<div>
-		{#each articles as article}
-			<article>
-				{article}
-			</article>
+	<div class="mt-8">
+		{#if articles.length > 0}
+			<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+				{#each articles as { title, slug, date, formattedDate }}
+					<a href={`/blog/${slug}`}>
+						<article class="">
+							<time datetime={date}>{formattedDate}</time>
+
+							{title}
+						</article>
+					</a>
+				{/each}
+			</div>
 		{:else}
 			<div class="flex flex-col items-center mt-6">
 				<p class="text-xl font-medium">I have not written about this topic yet.</p>
 
 				<a href="/blog" class="mt-4">Go to writing principal page</a>
 			</div>
-		{/each}
+		{/if}
 	</div>
 </Page>
--- src/routes/blog/topic/[topic].svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-
-	$: topic = $page.params.topic;
-</script>
-
-<p>Topic: {topic}</p>
