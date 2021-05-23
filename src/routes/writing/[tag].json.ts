e7afbfb9370f11a4c47383439f4bff7d421a5322
Devessier <baptiste@devessier.fr>
2021-05-23 23:26:54 +0200

    refactor: deduplicate blog posts fetching

--- src/routes/writing/[tag].json.ts
+++ src/routes/writing/[tag].json.ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import type { Locals, WritingPreview } from '$lib/types';
-import { basename } from 'path';
 import slugify from 'slugify';
+import { getBlogPosts } from './_api';
 
 interface GetResult {
 	prettyTagName?: string;
@@ -9,24 +9,12 @@ interface GetResult {
 }
 
 export const get: RequestHandler<Locals, GetResult> = async ({ params: { tag } }) => {
-	const svxFiles = import.meta.globEager('./*.svx');
-
-	const importedFiles: WritingPreview[] = Object.entries(svxFiles).map(([path, { metadata }]) => {
-		const { title, description, datetime, tags } = metadata as Omit<WritingPreview, 'slug'>;
-
-		return {
-			title,
-			description,
-			datetime,
-			tags,
-			slug: basename(path, '.svx')
-		};
-	});
+	const blogPosts = getBlogPosts();
 
 	const articles: WritingPreview[] = [];
 	let prettyTagName: string | undefined;
 
-	for (const blogPost of importedFiles) {
+	for (const blogPost of blogPosts) {
 		for (const blogPostTag of blogPost.tags) {
 			const sluggifiedTag = slugify(blogPostTag, {
 				lower: true
--- src/routes/writing/index.json.ts
+++ src/routes/writing/index.json.ts
@@ -1,29 +1,17 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import type { Locals, WritingPreview } from '$lib/types';
-import { basename } from 'path';
+import { getBlogPosts } from './_api';
 
 interface GetResult {
 	articles: WritingPreview[];
 }
 
 export const get: RequestHandler<Locals, GetResult> = async () => {
-	const svxFiles = import.meta.globEager('./*.svx');
-
-	const importedFiles: WritingPreview[] = Object.entries(svxFiles).map(([path, { metadata }]) => {
-		const { title, description, datetime, tags } = metadata as Omit<WritingPreview, 'slug'>;
-
-		return {
-			title,
-			description,
-			datetime,
-			tags,
-			slug: basename(path, '.svx')
-		};
-	});
+	const blogPosts = getBlogPosts();
 
 	return {
 		body: {
-			articles: importedFiles
+			articles: blogPosts
 		}
 	};
 };
