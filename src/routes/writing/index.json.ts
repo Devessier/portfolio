fc65478d87a4f1722037988e10c7781d7f879e9b
Devessier <baptiste@devessier.fr>
2021-05-23 22:24:24 +0200

    fix: use import.meta.globEager to get all blog posts
    We do not need to get posts content and then parse them to extract
    frontmatter data, mdsvex exposes those data when it compiles
    a component.

--- src/routes/writing/index.json.ts
+++ src/routes/writing/index.json.ts
@@ -1,31 +1,23 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import type { Locals, WritingPreview } from '$lib/types';
-import { readdirSync, readFileSync } from 'fs';
-import { join } from 'path';
-import matter from 'gray-matter';
+import { basename } from 'path';
 
 interface GetResult {
 	articles: WritingPreview[];
 }
 
 export const get: RequestHandler<Locals, GetResult> = async () => {
-	const currentDirectoryEntries = readdirSync(join(import.meta.url, '..'));
-	const svxFiles = currentDirectoryEntries.filter((entry) => entry.endsWith('.svx'));
+	const svxFiles = import.meta.globEager('./*.svx');
 
-	const importedFiles: WritingPreview[] = svxFiles.map((path) => {
-		const fileContent = readFileSync(join(import.meta.url, '..', path));
-		const parsingResult = matter(fileContent);
-		const { title, description, datetime, tags } = parsingResult.data as Omit<
-			WritingPreview,
-			'slug'
-		>;
+	const importedFiles: WritingPreview[] = Object.entries(svxFiles).map(([path, { metadata }]) => {
+		const { title, description, datetime, tags } = metadata as Omit<WritingPreview, 'slug'>;
 
 		return {
 			title,
 			description,
 			datetime,
 			tags,
-			slug: path.replace('.svx', '')
+			slug: basename(path, '.svx')
 		};
 	});
 
