9790764382d4f13a8815277d622e3a027f555fce
Devessier <baptiste@devessier.fr>
2021-05-23 16:09:38 +0200

    feat: configure mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,19 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.svx'],
+
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
 		preprocess({
 			postcss: true
-		})
+		}),
+
+		mdsvex()
 	],
 
 	kit: {
