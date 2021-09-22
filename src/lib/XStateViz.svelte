697eff1f0255ed67a868811515dd88c8ec5cf873
Devessier <baptiste@devessier.fr>
2021-09-23 00:15:55 +0200

    feat: create a component that renders Stately visualizer inside an inframe

---
+++ src/lib/XStateViz.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let id;
+
+	$: src = `https://stately.ai/viz/${id}`;
+</script>
+
+<div class="aspect-w-16 aspect-h-9">
+	<iframe {src} title="XState Vizualiser" class="w-full h-full" />
+</div>
