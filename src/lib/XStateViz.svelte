032a83adfc372662055d89ae3a931cb79aa873b7
Devessier <baptiste@devessier.fr>
2021-09-24 12:35:40 +0200

    feat: use new embed mode of Stately viz :tada:
    This mode has just been released.

--- src/lib/XStateViz.svelte
+++ src/lib/XStateViz.svelte
@@ -1,9 +1,15 @@
 <script>
 	export let id;
 
-	$: src = `https://stately.ai/viz/${id}`;
+	$: src = `https://stately.ai/viz/embed/${id}?mode=viz&controls=1&pan=0&zoom=0`;
 </script>
 
 <div class="aspect-w-16 aspect-h-9">
-	<iframe {src} title="XState Vizualiser" class="w-full h-full" />
+	<iframe
+		{src}
+		title="XState Vizualiser"
+		allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+		sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+		class="w-full h-full"
+	/>
 </div>
