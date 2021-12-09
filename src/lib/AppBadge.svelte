e9fea84c152b40cc08364cc6a1de1a42de4f710e
Devessier <baptiste@devessier.fr>
2021-12-09 20:40:30 +0100

    feat: make AppBadge able to show a link only if necessary

--- src/lib/AppBadge.svelte
+++ src/lib/AppBadge.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
-	export let href: string;
+	export let href: string | undefined;
 
 	let className: string | undefined;
 	export { className as class };
+
+	$: rootClasses = `border-gray-300 mb-2 border text-gray-600 bg-white hover:bg-gray-50 transition-colors duration-100 ease-out font-normal text-xs px-3 py-1 rounded-2xl flex-shrink-0 mx-1 ${className}`;
 </script>
 
-<a
-	{href}
-	sveltekit:prefetch
-	class="border-gray-300 mb-2 border text-gray-600 bg-white hover:bg-gray-50 transition-colors duration-100 ease-out font-normal text-xs px-3 py-1 rounded-2xl flex-shrink-0 mx-1 {className}"
->
-	<slot />
-</a>
+{#if href !== undefined}
+	<a {href} sveltekit:prefetch class={rootClasses}>
+		<slot />
+	</a>
+{:else}
+	<span class={rootClasses}>
+		<slot />
+	</span>
+{/if}
