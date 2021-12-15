a6b0e91f91622bce20c4860eee666977174c1002
Devessier <baptiste@devessier.fr>
2021-12-16 00:15:10 +0100

    build: create drone command to set article datetime to now

--- drone.js
+++ drone.js
@@ -1,7 +1,7 @@
 import sade from 'sade';
 import dedent from 'dedent';
 import slugify from 'slugify';
-import { writeFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
 const prog = sade('drone');
@@ -23,12 +23,13 @@ Hi from **${blogPostName}** article!
 	);
 }
 
+const WRITING_DIRECTORY = new URL('src/routes/writing', import.meta.url);
+
 prog
 	.command('generate-article <title>')
 	.describe('Scaffold a blog post using a predefined template')
 	.example("generate-article 'My journey as a Vue.js developer'")
 	.action(async (title) => {
-		const WRITING_DIRECTORY = new URL('src/routes/writing', import.meta.url);
 		const template = generateBlogPostTemplate(title);
 		const slug = slugify(title, {
 			lower: true
@@ -39,6 +40,31 @@ prog
 		await writeFile(blogPostPath, template);
 
 		console.log(`Blog post has been generated at ${blogPostPath}`);
+	});
+
+prog
+	.command('update-article-datetime <title>')
+	.describe('Update the datetime of a blog post')
+	.example("update-article-datetime 'My journey as a Vue.js developer'")
+	.action(async (title) => {
+		const filePath = join(WRITING_DIRECTORY.pathname, `${title}.svx`);
+		const fileContent = await readFile(filePath, 'utf8');
+		const fileContentLines = fileContent.split('\n');
+
+		const datetimeLine = fileContentLines.find((line) => line.includes('datetime:'));
+		if (datetimeLine !== undefined) {
+			const currentDateTime = new Date().toISOString();
+
+			const newFileContent = fileContentLines.map((line) => {
+				if (line.startsWith('datetime:')) {
+					return `datetime: ${currentDateTime}`;
+				}
+
+				return line;
+			});
+
+			await writeFile(filePath, newFileContent.join('\n'));
+		}
 	});
 
 prog.parse(process.argv);
