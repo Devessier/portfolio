9b0007a71df4abfef6fcea22a012c27ef59b69da
Devessier <baptiste@devessier.fr>
2021-12-22 00:37:04 +0100

    feat: create endpoint that generates rss feed

---
+++ src/routes/feed.xml.ts
@@ -0,0 +1,45 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import { Feed } from 'feed';
+import urlcat from 'urlcat';
+import type { Locals } from '$lib/types';
+import { APP_URL } from '$lib/env';
+import { getBlogPosts } from './writing/_api';
+
+export const get: RequestHandler<Locals, unknown, string> = () => {
+	const blogPosts = getBlogPosts();
+
+	const feed = new Feed({
+		title: 'Baptiste Devessier',
+		description: "Baptiste Devessier's blog",
+		id: APP_URL,
+		link: APP_URL,
+		language: 'en',
+		feedLinks: {
+			atom: urlcat(APP_URL, '/feed.xml')
+		},
+		copyright: '',
+		author: {
+			name: 'Baptiste Devessier',
+			email: 'baptiste@devessier.fr'
+		}
+	});
+
+	for (const blogPost of blogPosts) {
+		const href = urlcat(APP_URL, `/writing/${blogPost.slug}/`);
+
+		feed.addItem({
+			id: href,
+			title: blogPost.title,
+			link: href,
+			date: new Date(blogPost.datetime),
+			description: blogPost.description
+		});
+	}
+
+	return {
+		headers: {
+			'Content-Type': 'application/xml'
+		},
+		body: feed.atom1()
+	};
+};
