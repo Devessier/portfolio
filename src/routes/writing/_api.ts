f0f14db27a96adbad0149e8bc77175bc3c2604c7
Devessier <baptiste@devessier.fr>
2022-08-02 20:05:47 +0200

    feat: hide articles of serie except first one as they need to be finished

--- src/routes/writing/_api.ts
+++ src/routes/writing/_api.ts
@@ -5,6 +5,12 @@ export function getBlogPosts(): WritingPreview[] {
 	const svxFiles = import.meta.globEager('./*.svx');
 
 	const importedFiles: WritingPreview[] = Object.entries(svxFiles)
+		.filter(([path]) => {
+			const fileIsHidden = basename(path).startsWith('_');
+			const shouldKeepFile = fileIsHidden === false;
+
+			return shouldKeepFile === true;
+		})
 		.map(([path, { metadata }]) => {
 			const { title, description, datetime, tags } = metadata as Omit<WritingPreview, 'slug'>;
 
