30ce257c2a48fa1b838f2f78f4a6bb858c8ca98c
Devessier <baptiste@devessier.fr>
2022-10-16 18:38:47 +0200

    fix: make all parts of note page equal width

--- src/routes/note/[slug]/+page.svelte
+++ src/routes/note/[slug]/+page.svelte
@@ -39,24 +39,24 @@
 </script>
 
 <Page title={pageTitle} {description} {canonical} {schemas} {facebook} {twitter}>
-	<div class="max-w-prose mx-auto">
-		<div class="text-lg">
-			<time {datetime} class="block mb-4 text-red-700 text-center italic font-medium text-sm">
-				{formattedDatetime}
-			</time>
+	<div class="max-w-prose text-lg mx-auto w-full">
+		<time {datetime} class="block mb-4 text-red-700 text-center italic font-medium text-sm">
+			{formattedDatetime}
+		</time>
 
-			<h1
-				class="text-3xl text-center font-cursive leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl"
-			>
-				{title}
-			</h1>
-		</div>
+		<h1
+			class="text-3xl text-center font-cursive leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl"
+		>
+			{title}
+		</h1>
+	</div>
 
-		<div class="mt-6 prose prose-red prose-lg text-gray-500 mx-auto break-words">
-			<svelte:component this={Content} />
-		</div>
+	<div class="mt-6 prose prose-red prose-lg text-gray-500 mx-auto break-words">
+		<svelte:component this={Content} />
+	</div>
 
-		<div class="mt-12">
+	<div class="max-w-prose text-lg mx-auto mt-12 w-full">
+		<div class="text-base">
 			<LinkWithAnimatedArrow href="/notes/">Go back to all notes</LinkWithAnimatedArrow>
 		</div>
 	</div>
