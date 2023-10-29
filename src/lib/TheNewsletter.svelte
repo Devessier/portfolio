bc5679ffcbd652a222275729445196678389c7d5
Devessier <baptiste@devessier.fr>
2023-10-29 23:17:46 +0100

    feat: add label about data privacy

--- src/lib/TheNewsletter.svelte
+++ src/lib/TheNewsletter.svelte
@@ -30,7 +30,7 @@
 				on:submit={() => {
 					window.open('https://newsletter.baptiste.devessier.fr', 'popupwindow');
 				}}
-				class="mx-auto mt-10 flex flex-col sm:flex-row max-w-md gap-x-4 gap-y-2"
+				class="mx-auto mt-10 grid grid-cols-1 sm:grid-cols-[1fr,auto] max-w-md gap-x-4 gap-y-2"
 			>
 				<label for="email-address" class="sr-only">Email address</label>
 				<input
@@ -41,6 +41,7 @@
 					required
 					class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
 					placeholder="Enter your email"
+					aria-describedby="email-description"
 				/>
 				<button
 					type="submit"
@@ -48,6 +49,9 @@
 				>
 					Subscribe
 				</button>
+				<p class="text-sm text-gray-400 sm:col-span-2 text-center" id="email-description">
+					I value your privacy and will never share your email address.
+				</p>
 			</form>
 			<svg
 				viewBox="0 0 1024 1024"
