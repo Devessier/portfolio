543bb6027bc3294e70fd32e767daebc7f6c24e6b
Devessier <baptiste@devessier.fr>
2023-11-22 20:48:55 +0100

    feat: vendor lite-youtube-embed and create reusable component

---
+++ src/lib/YouTubePlayer.svelte
@@ -0,0 +1,22 @@
+<script context="module" lang="ts">
+	let inserted = false;
+</script>
+
+<script lang="ts">
+	let shouldInsert = !inserted;
+	inserted = true;
+
+	export let videoId: string;
+</script>
+
+<svelte:head>
+	{#if shouldInsert}
+		<link rel="stylesheet" href="/lite-youtube-embed/styles.css" />
+
+		<script src="/lite-youtube-embed/script.js"></script>
+	{/if}
+</svelte:head>
+
+<div class="grid grid-cols-1 aspect-video w-full">
+    <lite-youtube videoid={videoId} />
+</div>
