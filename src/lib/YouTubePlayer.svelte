fb1f1a43669a58122aa96fac11ec9b7e664b4230
Devessier <baptiste@devessier.fr>
2023-11-22 21:03:47 +0100

    fix: make youtube player work constantly

--- src/lib/YouTubePlayer.svelte
+++ src/lib/YouTubePlayer.svelte
@@ -1,22 +1,7 @@
-<script context="module" lang="ts">
-	let inserted = false;
-</script>
-
 <script lang="ts">
-	let shouldInsert = !inserted;
-	inserted = true;
-
 	export let videoId: string;
 </script>
 
-<svelte:head>
-	{#if shouldInsert}
-		<link rel="stylesheet" href="/lite-youtube-embed/styles.css" />
-
-		<script src="/lite-youtube-embed/script.js"></script>
-	{/if}
-</svelte:head>
-
 <div class="grid grid-cols-1 aspect-video w-full">
-    <lite-youtube videoid={videoId} />
+	{@html `<lite-youtube videoid="${videoId}"></lite-youtube>`}
 </div>
