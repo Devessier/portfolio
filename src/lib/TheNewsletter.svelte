8fecbc92ba543e8637cfc0be68800b03720b5080
Devessier <baptiste@devessier.fr>
2024-06-05 12:01:45 +0200

    feat: reduce the height of the newsletter section

--- src/lib/TheNewsletter.svelte
+++ src/lib/TheNewsletter.svelte
@@ -5,7 +5,7 @@
 <div class="bg-white py-16 sm:py-32 -mx-6 sm:mx-0">
 	<div class="mx-auto">
 		<div
-			class="relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32"
+			class="relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24"
 		>
 			<h2
 				class="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl"
