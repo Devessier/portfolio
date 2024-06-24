9e59c8f9cc55c5f09d83904b507a5c0ffb5d6f62
Devessier <baptiste@devessier.fr>
2024-06-24 18:22:07 +0200

    feat: allow the youtube player to display a title

--- src/lib/YouTubePlayer.svelte
+++ src/lib/YouTubePlayer.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	export let videoId: string;
+	export let videoTitle: string | undefined = undefined;
 	export let thumbnailQuality: 'low' | 'high' | undefined = 'low';
 </script>
 
@@ -7,6 +8,7 @@
 	{@html `
 		<lite-youtube
 			videoid="${videoId}"
+			${videoTitle === undefined ? '' : `title="${videoTitle}"`}
 			style="${
 				thumbnailQuality === 'high'
 					? `background-image: url('https://i.ytimg.com/vi/${videoId}/sddefault.jpg');`
