2f67d84bc0aa04367566ee752fde9f73de09d9d9
Devessier <baptiste@devessier.fr>
2024-10-24 22:45:31 +0200

    feat: use darker red in prose content and remove ugly backticks for code snippets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,27 @@ module.exports = {
 			fontFamily: {
 				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
 				cursive: ['Comfortaa', 'cursive']
-			}
+			},
+			typography: (theme) => ({
+				DEFAULT: {
+					css: {
+						'code::before': {
+							content: 'unset'
+						},
+						'code::after': {
+							content: 'unset'
+						},
+						code: {
+							color: theme('colors.red.700')
+						}
+					}
+				},
+				red: {
+					css: {
+						'--tw-prose-links': theme('colors.red.700')
+					}
+				}
+			})
 		}
 	},
 	plugins: [require('@tailwindcss/typography')]
